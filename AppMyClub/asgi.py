9841814de23ea5c39aa6870561edc5e7db8eb29c
Natalia Roldan <128727627+NataliaRoldan165@users.noreply.github.com>
2023-09-11 22:38:24 -0300

    Subida de archivos

---
+++ AppMyClub/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for AppMyClub project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppMyClub.settings')
+
+application = get_asgi_application()
---
+++ core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index (request):
+    return HttpResponse("Bienvenidos al Sistema Gestión para clubes")
+
+def actividades (request):
+    return HttpResponse("""
+<ul>
+    <li>Futbol</li>
+    <li>Vóley</li>
+    <li>Patín</li>
+</ul> 
+""")
+
