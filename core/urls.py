ec1bc851d186bbd587ad29fd30cd3abfc112b311
Guilhe3p <guilhermerutenberg@gmail.com>
2023-09-19 00:03:55 -0300

    probando merge devG y devN a main

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), 
-    path('actividades/listado',views.actividades, name='AppMyClub_actividades'),
-
+    path('institucional',views.institucional, name='AppMyClub_institucional'),
+    path('socios/login',views.socios, name='Socios_login'),
+    path('contacto',views.contacto, name='AppMyClub_contacto'),
+    path('actividades',views.actividades, name='actividades'),
+    path('actividades/<str:actividad>', views.actividad, name="detalle_actividad")
 
 ]
--- core/views.py
+++ core/views.py
@@ -5,8 +5,27 @@ from django.template import loader
 
 
 # Create your views here.
-def index (request):
-    return render(request, "core/index.html")
+def index(request):
+    context = {
+        'menu_sports' : [
+            {'name':'futbol','url_image':'core/img/pibe_pelota.jpg'},
+            {'name':'voley','url_image':'core/img/volley.jpg'},
+            {'name':'basket','url_image':'core/img/basketball.jpg'},
+            {'name':'tenis','url_image':'core/img/tenis.jpg'},
+            {'name':'natación','url_image':'core/img/natacion.jpg'},
+            {'name':'handball','url_image':'core/img/handball.jpg'}
+        ],
+        'menu_activities' : [
+            {'name':'yoga','url_image':'core/img/yoga.jpg'},
+            {'name':'expresión','url_image':'core/img/expresion.jpeg'},
+            {'name':'parrilla','url_image':'core/img/parrilla.jpeg'},
+            {'name':'colonia','url_image':'core/img/colonia.jpg'},
+            {'name':'eventos','url_image':'core/img/recital.jpg'},
+            {'name':'salón','url_image':'core/img/salon.jpeg'}
+        ]
+    }
+
+    return render(request,"core/index.html",context)
 
 def actividades (request):
     return HttpResponse("""
