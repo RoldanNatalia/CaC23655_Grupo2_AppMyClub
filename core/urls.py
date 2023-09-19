23910d66e2790f28476ece70c7a4bb577c16d777
Natalia Roldan <128727627+NataliaRoldan165@users.noreply.github.com>
2023-09-18 23:30:07 -0300

    Add contacto.html, base_n.html,styles_base.css

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), 
+    path('institucional',views.institucional, name='AppMyClub_institucional'),
     path('actividades/listado',views.actividades, name='AppMyClub_actividades'),
-
+    path('socios/login',views.socios, name='Socios_login'),
+    path('contacto',views.contacto, name='AppMyClub_contacto'),
 
 ]
