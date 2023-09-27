9270125cffb892c760f7dee8a86c90ed416f53b1
Natalia Roldan <128727627+NataliaRoldan165@users.noreply.github.com>
2023-09-27 09:49:48 -0300

    actualice para hacer pull

--- core/views.py
+++ core/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+ 
 
 
 # Create your views here.
 def index (request):
-    return HttpResponse("Bienvenidos al Sistema Gestión para clubes")
+    return render(request, "core/index.html")
 
 def actividades (request):
-    return HttpResponse("""
-<ul>
-    <li>Futbol</li>
-    <li>Vóley</li>
-    <li>Patín</li>
-</ul> 
-""")
+    return render (request, "core/actividades.html")
 
+def institucional (request):
+    return render (request, "core/institucional.html")
+
+def socios (request):
+    return render (request,"core/socios.html")
+
+def contacto (request):
+    return render (request, "core/contacto.html")
