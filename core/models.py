e0969fc45e9f6863d76d0db51fd3dfd748cc9e0c
juan-19t <juantrentino@gmail.com>
2023-10-05 11:23:14 +0200

    depurando para hacer pull

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=60)
+    dni=models.IntegerField
+    email=models.EmailField
+    direccion=models.CharField(max_length=100)
