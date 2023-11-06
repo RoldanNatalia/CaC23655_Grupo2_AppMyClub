971fc6281644153cfe2e61286fdd4f9ab38e14e9
Guilhe3p <guilhermerutenberg@gmail.com>
2023-11-06 18:10:12 -0300

    django admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from core.models import Socio, Profesor, Curso, Predio, Actividad, Inscripcion, DiaCurso
 
 # Register your models here.
+
+class SocioAdmin(admin.ModelAdmin):
+    list_display = ('numero','nombre','apellido','dni')
+    list_editable = ('nombre','apellido','dni')
+
+admin.site.register(Socio, SocioAdmin)
+admin.site.register(Profesor)
+admin.site.register(Curso)
+admin.site.register(Predio)
+admin.site.register(Actividad)
+admin.site.register(Inscripcion)
+admin.site.register(DiaCurso)
+
+
+
